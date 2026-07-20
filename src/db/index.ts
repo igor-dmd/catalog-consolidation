cac48d729b2213c3f3c4ca23508c94ccbaae0d5d
Igor de Medeiros <igormedeiros0@gmail.com>
2026-07-20 20:21:49 -0300

    Clean up scaffold leftovers

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,16 +4,4 @@ export {
   insertSellerProductLink,
   listCatalogProducts
 } from "./catalog-repository.js";
-export {
-  catalogProductFromRow,
-  catalogProductToInsertParams,
-  sellerProductLinkFromRow,
-  sellerProductLinkToInsertParams
-} from "./mappers.js";
 export { runCatalogMigrations } from "./migrations/runner.js";
-export type {
-  CatalogProductInsertParams,
-  CatalogProductRow,
-  SellerProductLinkInsertParams,
-  SellerProductLinkRow
-} from "./schema.js";
